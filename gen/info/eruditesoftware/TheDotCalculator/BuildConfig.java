1b638eed214dca4e01b9d00361d0be07c50c2b34
dylan <dylan@eruditesoftware.info>
2012-09-30 12:55:55 -0500

    removed offending files

--- gen/info/eruditesoftware/TheDotCalculator/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package info.eruditesoftware.TheDotCalculator;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
